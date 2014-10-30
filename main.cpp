a3834f9b8fd578aa93fdf0e52ba0e02a75ee2fea
Mutthoju B <brahmaji.mutthoju@jpmchase.com>
2014-10-29 23:05:57 -0400

    setup: main function added end of line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout<<"AlhamduliLLAHi RABBil alameen";
+    std::cout<<"AlhamduliLLAHi RABBil alameen"<<std::endl;
     return 0;
 }
